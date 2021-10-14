87e25f76f0ec8f506189a77593b794c715970d30
ian <licitdev@gmail.com>
2021-10-14 15:08:48 +0800

    Add admin manual worker update

--- worker/index.js
+++ worker/index.js
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import jwt from '@tsndr/cloudflare-worker-jwt';
 
 addEventListener('fetch', async (event) => {
 	const { request } = event;
@@ -6,6 +7,8 @@ addEventListener('fetch', async (event) => {
 
 	if (request.method === 'OPTIONS') {
 		return event.respondWith(handleOptions(request));
+	} else if (request.method === 'POST' && url.includes('/supaflare_cfw_admin_update')) {
+		return event.respondWith(processUpdateAdmin(request));
 	} else if (request.method === 'POST' && url.includes('/supaflare_cfw_update')) {
 		return event.respondWith(processUpdate(request));
 	} else {
@@ -30,14 +33,7 @@ async function processUpdate(request) {
 			supabase.auth.setAuth(requestData.token);
 			const { data, error } = await supabase.from('links').select('*').eq('id', requestData.link_id);
 			if (error) throw error;
-			if (data.length === 0) {
-				const linkData = await SUPAFLARE.get('links:slug/' + requestData.slug, { type: 'json' });
-				if (linkData && linkData.id === requestData.link_id) {
-					await SUPAFLARE.delete('links:slug/' + requestData.slug);
-				}
-			} else {
-				await SUPAFLARE.put('links:slug/' + data[0].slug, JSON.stringify(data[0]));
-			}
+			await updateKV(requestData, data);
 			const response = new Response('OK');
 			response.headers.set('Access-Control-Allow-Origin', new URL(request.headers.get('origin')));
 			return response;
@@ -52,6 +48,64 @@ async function processUpdate(request) {
 	return new Response('Forbidden.', {
 		status: 403,
 	});
+}
+
+async function processUpdateAdmin(request) {
+	const { headers } = request;
+	const contentType = headers.get('content-type') || '';
+	if (contentType.includes('application/json')) {
+		try {
+			const requestData = await request.json();
+			if (
+				!requestData.admin_key ||
+				requestData.admin_key !== SUPAFLARE_ADMIN_KEY ||
+				!requestData.token ||
+				!requestData.link_id ||
+				!/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(requestData.link_id)
+			) {
+				throw Error;
+			}
+			const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+			if (!(await jwt.verify(requestData.token, SUPABASE_JWT_SECRET))) throw Error;
+			let decodedToken = jwt.decode(requestData.token);
+			decodedToken.role = 'service_role';
+			const serviceRoleToken = await jwt.sign(decodedToken, SUPABASE_JWT_SECRET);
+			supabase.auth.setAuth(serviceRoleToken);
+			const { data, error } = await supabase.from('links').select('*').eq('id', requestData.link_id);
+			if (error) throw error;
+			await updateKV(requestData, data);
+			const response = new Response('OK');
+			response.headers.set('Access-Control-Allow-Origin', new URL(request.headers.get('origin')));
+			return response;
+		} catch (error) {
+			const response = new Response('Server Error.', {
+				status: 500,
+			});
+			response.headers.set('Access-Control-Allow-Origin', new URL(request.headers.get('origin')));
+			return response;
+		}
+	}
+	return new Response('Forbidden.', {
+		status: 403,
+	});
+}
+
+async function updateKV(requestData, data) {
+	if (data.length === 0) {
+		const currentSlug = await SUPAFLARE.get('links:id/' + requestData.link_id, { type: 'text' });
+		const linkData = await SUPAFLARE.get('links:slug/' + currentSlug, { type: 'json' });
+		if (linkData && linkData.id === requestData.link_id) {
+			await SUPAFLARE.delete('links:slug/' + linkData.slug);
+			await SUPAFLARE.delete('links:id/' + linkData.id);
+		}
+	} else {
+		const currentSlug = await SUPAFLARE.get('links:id/' + requestData.link_id, { type: 'text' });
+		await SUPAFLARE.put('links:slug/' + data[0].slug, JSON.stringify(data[0]));
+		if (data[0].slug !== currentSlug) {
+			await SUPAFLARE.put('links:id/' + requestData.link_id, data[0].slug);
+			await SUPAFLARE.delete('links:slug/' + currentSlug);
+		}
+	}
 }
 
 async function processRedirect(request) {
